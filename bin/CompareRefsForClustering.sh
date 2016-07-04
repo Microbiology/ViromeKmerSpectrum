9f27c952386ca2fd67f8ce0db2c90d79d08ad8f1
Geoffrey Hannigan <ghanni@upenn.edu>
2016-07-04 09:14:08 -0400

    Running somparison on axiom

--- bin/CompareRefsForClustering.sh
+++ bin/CompareRefsForClustering.sh
@@ -11,19 +11,20 @@ export Output='CompareRefs'
 export LocalPath=/Users/Hannigan/git/ViromeKmerSpectrum/bin/
 export FigurePath=/Users/Hannigan/git/ViromeKmerSpectrum/Figures/
 
-export Genomes=/Users/Hannigan/git/ViromeKmerSpectrum/data/tmpbench/subsample.fa
+export Genomes=/Users/Hannigan/git/ViromeKmerSpectrum/data/PhageSVAFormat.fa
+
+# Load in the proper perl module
+module load perl/5.22.1 
 
 # Move and make output dir
 cd ${WorkingDirectory} || exit
 
 mkdir ./${Output}
 
-perl ../bin/CalculateKmerDistancesPar.pl \
+perl ../bin/CalculateKmerDistances.pl \
 	-i ${Genomes} \
 	-t ${Genomes} \
 	-o ./${Output}/RefCompare.tsv \
 	-f ./${Output}/RefCompareFormat.tsv \
 	-w 5 \
-	-p 32 \
 	-r
-
